5628299f0958e84c7e726bd6fec3761189fc559c
Bhavdeep <bhavdeep@rrc.ca>
2024-02-21 13:50:35 -0600

    Setting up the incorporate transactions.

--- bank_transactions.py
+++ bank_transactions.py
@@ -13,7 +13,7 @@ import locale
 locale .setlocale(locale.LC_ALL, '')
 
 # Defining transaction options
-transactions_options = {"D", "W", "Q"}
+transaction_options = {"D", "W", "Q"}
 
 # Generating random balance
 random_balance = float(random.randint(-1000, 10000))
@@ -30,4 +30,35 @@ print("{:^40}".format("Desposit:D"))
 print("{:^40}".format("Withdraw:W"))
 print("{:^40}".format("Quit:Q"))
 print("*" *40)
+
+# Main program loop
+while True:
+    user_selection = input("Enter your selection: ").upper()
+ 
+    if user_selection not in transaction_options:
+        print("*" * 40)
+        print("{:^40}".format("INVALID SELECTION"))
+        print("*" * 40)
+    elif user_selection == "D":
+        deposit_amount = float(input("Enter amount of transaction: "))
+        bank_balance += deposit_amount
+        print("*" * 40)
+        print("{:^40}".format(f"Your current balance is: {locale.currency(bank_balance, grouping=True)}"))
+        print("*" * 40)
+    elif user_selection == "W":
+        withdraw_amount = float(input("Enter amount of trnsaction: "))
+        if withdraw_amount > bank_balance:
+            print("*" * 40)
+            print("{:^40}".formt("INSUFFICIENT FUNDS"))
+            print("*" * 40)
+        else:
+            bank_balance -= withdraw_amount
+            print("*" * 40)
+            print("{:^40}".format(f"Your current balance is: {locale.currency(bank_balance, grouping=True)}"))
+            print("*" * 40)
+    elif user_selection == "Q":
+         pass
+    else:
+        print("*" * 40)
+        print("{:^40}".format("INVALID SELECTION"))
 
